e6060006189247fb1fc557709c56c63fce4a40ec
gautamprajapat <gautamprajapati052@gmail.com>
2024-04-20 16:48:36 +0530

    vite project added

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+import Chai from "./Chai"
+
+function App() {
+  const username="Gautam prajapati";
+
+  return (
+  
+    <>
+     <Chai/>
+     <h1>This is vite react project with {username}</h1>
+     <p>test para</p>
+     </>
+   
+  )
+}
+
+export default App
